25ffe10faf8c1d0a377b48a656df46afb5b83287
David Yoo <daooyoo@gmail.com>
2016-09-15 18:17:56 -0500

    created build-a-blog

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import os
 import jinja2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
@@ -16,8 +18,45 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write("Hello World!")
+        self.redirect('/blog')
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class BlogPosts(Handler):
+    def get(self):
+        posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5")
+        self.render("blog.html", posts = posts)
+
+class NewPost(Handler):
+    def render_newpost(self, title = "", content = "", error = ""):
+        self.render("newpost.html", title = title, content = content, error = error)
+
+    def get(self):
+        self.render_newpost()
+
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+
+        if title and content:
+            post = BlogPost(title = title, content = content)
+            post.put()
+            self.redirect("/blog")
+        else:
+            error = "we need both a title and some content"
+            self.render_newpost(title = title, content = content, error = error)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        post = BlogPost.get_by_id(int(id))
+        self.render("post.html", post = post)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/blog', BlogPosts),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
